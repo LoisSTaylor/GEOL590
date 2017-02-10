73dae1234dd9972f0b6610d7b8bca30964fd4867
L. Stacy Taylor <ltaylo32@vols.utk.edu>
2017-02-10 15:01:24 -0500

    nematode file can update

---
+++ R/task5.R
@@ -0,0 +1,7 @@
+# This is a test file from a class I have this semester
+#We are adopting two populations of nematodes and running plate counts daily
+#Ultimately, I'd like to continually update this file to Github as well as...
+#....playing with ggplot as the data evolves and discovering the quirks of the system!
+# You're going to see a LOT of this file...
+
+finding_nemo <- read.csv(file="/Users/LStacyT/Desktop/GEOL_590/Nematode_counts_2017_updatable.csv",skip = 2)
